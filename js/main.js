3f33a881fa878ccf1a532e54c2161471233f857b
nachobaile <nachobaile@gmail.com>
2018-01-24 14:37:09 +0100

    rebote contra las paredes

--- js/main.js
+++ js/main.js
@@ -5,31 +5,32 @@ $(document).ready(function() {
 
   var jugador1 = new Jugador(70, 0,25, [81,65],size); // Q,A
   var jugador2 = new Jugador(1200, 500, 25, [38,40],size); // Arrow UP, Aqrrow Down
-  var pelota= new Pelota(650,350,40,size);
+  var pelota= new Pelota(650,350,8,8,size);
   $(document).keydown(function(e) {
      
       jugador1.key(e.keyCode);
       jugador2.key(e.keyCode);
   });
-function checkCollision(){
+// function checkCollision(){
 
-  if (jugador1.x < pelota.x + pelota.width  && jugador1.x + jugador1.width  > pelota.x &&
-    jugador1.y < pelota.y + pelota.height && jugador1.y + jugador1.height > pelota.y) {
-// The objects are touching
-}
-  if(jugador2.x < pelota.x + pelota.width  && jugador2.x + jugador2.width  > pelota.x &&
-    jugador2.y < pelota.y + pelota.height && jugador2.y + jugador2.height > pelota.y) {
-// The objects are touching
-}
-}
+//   if (jugador1.x < pelota.x + pelota.width  && jugador1.x + jugador1.width  > pelota.x &&
+//     jugador1.y < pelota.y + pelota.height && jugador1.y + jugador1.height > pelota.y) {
+// // The objects are touching
+// }
+//   if(jugador2.x < pelota.x + pelota.width  && jugador2.x + jugador2.width  > pelota.x &&
+//     jugador2.y < pelota.y + pelota.height && jugador2.y + jugador2.height > pelota.y) {
+// // The objects are touching
+// }
+// }
 
 
   setInterval(update, 1000 / 60);
   function update() {
     jugador1.render();
     jugador2.render();
-    pelota.render();
     pelota.move();
+    pelota.render();
+    // pelota.move();
   } 
 
 });
--- js/pelota.js
+++ js/pelota.js
@@ -1,10 +1,11 @@
-function Pelota(x, y, speed,size) {
+function Pelota(x, y, sX, sY,size) {
     this.x = x;
     this.y = y;
-    this.speed = speed;
+    this.speedX = sX;
+    this.speedY = sY;
     this.size = size;
     this.element = $("<div>").attr("class", "pelota");
-    this.element.css({ top: this.y, left: this.x });
+    this.element.css({ top: this.y, left: this.x});
     $("#pantalla").append(this.element);
   }
 
@@ -12,9 +13,20 @@ function Pelota(x, y, speed,size) {
     this.element.css({ top: this.y });
   }
 
+  // Pelota.prototype.move = function(){
+  //   console.log('me muevo')
+  //   this.element.css({top:this.y--,left: this.x++})
+  // }
   Pelota.prototype.move = function(){
-    console.log('me muevo')
-    this.element.css({top:this.y--,left: this.x++})
+    if(this.x >= $("#pantalla").width() || this.x <= 0){
+      this.speedX *= -1;
+    }
+    if(this.y >= $("#pantalla").height() || this.y <= 0){
+      this.speedY *= -1;
+    }
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.element.css({top:this.y, left:this.x});
   }
   //    Pelota.prototype.move = function(){
      
